cad7adfe8da820d9fd935ff3f0f25600297fdd2e
Ignacio Elia <ielia@olenick.com>
2016-02-24 16:17:13 -0300

    Added echos.

--- fix-hosts-file.bat
+++ fix-hosts-file.bat
@@ -1,1 +1,4 @@
 echo 54.172.225.27 assets-01.ecm.hyattdev.com >> %WINDIR%\System32\drivers\etc\hosts
+echo ----- BOF: HOSTS -----
+cat %WINDIR%\System32\drivers\etc\hosts
+echo ----- EOF: HOSTS -----
--- fix-hosts-file.sh
+++ fix-hosts-file.sh
@@ -1,1 +1,4 @@
 echo 54.172.225.27 assets-01.ecm.hyattdev.com >> /etc/hosts
+echo ----- BOF: HOSTS -----
+cat /etc/hosts
+echo ----- EOF: HOSTS -----
