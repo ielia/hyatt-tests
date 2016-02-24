997a8a49bb9e447d9861e2727cf4711b96748e8d
Ignacio Elia <ielia@olenick.com>
2016-02-24 16:17:41 -0300

    Some style.

--- fix-hosts-file.sh
+++ fix-hosts-file.sh
@@ -1,4 +1,4 @@
-echo 54.172.225.27 assets-01.ecm.hyattdev.com >> /etc/hosts
+echo "54.172.225.27 assets-01.ecm.hyattdev.com" >> /etc/hosts
 echo ----- BOF: HOSTS -----
 cat /etc/hosts
 echo ----- EOF: HOSTS -----
